cd88d72b78871d6c6e2ba0bcf21495e5351e5ee6
Likk-0608 <sdielkk_6_8@163.com>
2022-09-05 22:51:41 +0800

    数据库语句

---
+++ tb_fincontent.sql
@@ -0,0 +1,8 @@
+drop table fincontent;
+CREATE TABLE fincontent (
+	id integer ,
+    stitle varchar(128),
+    scontent varchar(128)
+);
+INSERT INTO fincontent VALUES (1, '金融文章', '36氪');
+INSERT INTO fincontent VALUES (2, '雪球文章', '基金');
